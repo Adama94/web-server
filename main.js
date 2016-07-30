29f582fc2de0c5753b3e8e771e117f524d828ec7
EC2 Default User <ec2-user@ip-172-31-45-209.us-west-2.compute.internal>
2016-07-30 16:10:35 +0000

    Changed server to display in progress message

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!")
+var http = require("http");
+
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'})        
+   response.end('This page is a big work in progress. Check back later!\n');
+}).listen(80);
